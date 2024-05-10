461dc7a63032d0a008483a68e543754d6fd9b78b
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2024-05-10 15:23:03 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,11 +1,39 @@
 <?php
+// functions
+include "db.php";
 
-function puts($str){
-  echo $str;
+function _http_res($time, $dir){
+  echo "<meta http-equiv='refresh' content='{$time};url={$dir}'>";
 }
 
-function _viewport($size){
-  echo "<meta name='viewport' content='width=device-width, initial-scale=$size' />";
+function _innerjs($msg){
+  echo "<script>alert('$msg')</script>";
 }
 
-?>
+function _innerhtml(){
+  echo "
+   <style></style>
+   
+  ";
+}
+
+function _pictureHundler($connect, $style, $SqlPicture, $SqlPhone){
+  $picturesql = "select current_profile from users where phone = '$SqlPhone'";
+  $picturesqlq = mysqli_query($connect, $picturesql);
+  $picturesqlF = mysqli_fetch_assoc($picturesqlq);
+    $pictureVar = $picturesqlF["current_profile"];
+    
+    $customDesign = "<style>#image{ $style }</style>";
+   
+  if ($picturesqlF["current_profile"] == "") {
+    echo "{$customDesign} <img src='../main/xbin/img/sys/def.jpg' id='image' alt='defaultprofile'/>";
+  } else {
+    echo "{$customDesign} <img src='../main/xbin/img/users/$pictureVar' id='image' alt='$pictureVar'/>";
+  }
+  
+}
+
+
+
+
+
