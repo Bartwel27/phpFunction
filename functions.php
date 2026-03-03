73213c8569cd62dfefffd003eda734a9bb5319b9
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2026-03-03 23:42:20 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -284,5 +284,24 @@ function _error($msg) {
     ";
 }
 
+$invalidChars = " \"<>#%{}|\\^~[]`:\/?@!$&'()*+,;=";
+$validChars = "0123456789";
+$invalid_get_chars = [
 
+    // Not allowed directly in URLs
+    " ",  "\"", "<",  ">",  "#",  "%",  "{",  "}",  "|",
+    "\\", "^",  "~",  "[",  "]",  "`",
+    
+    // Reserved characters (must be encoded if used as data)
+    ":",  "/",  "?",  "@",  "!",  "$",  "&",  "'",
+    "(",  ")",  "*",  "+",  ",",  ";",  "=",
+    
+    // Lowercase letters
+    "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",
+    "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",
+    
+    // Uppercase letters
+    "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",
+    "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z",
 
+];
